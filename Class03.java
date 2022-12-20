dd0fdf36010711c56479741564560673c0156347
kuweidashi <89897887+kuweidashi@users.noreply.github.com>
2022-12-20 23:05:22 +0800

    Create Class03.java

---
+++ Class03.java
@@ -0,0 +1,15 @@
+package Java13;
+
+public class Class03 {
+    public static void main(String[] args) {
+        int num = 12, den = 0;
+        try {
+            int ans = num/den;
+            System.out.println("end of main()!!");
+        } catch (ArithmeticException e) {
+
+        } finally {
+            
+        }
+    }
+}
