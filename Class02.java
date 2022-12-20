edaa56708898cdbd35d5d3a33ce5f0c629107718
kuweidashi <89897887+kuweidashi@users.noreply.github.com>
2022-12-20 22:58:46 +0800

    Create Class02.java

---
+++ Class02.java
@@ -0,0 +1,13 @@
+package Java13;
+
+public class Class02 {
+    public static void main(String[] args) {
+        int num = 12, den = 0;
+        if (den == 0){
+            System.out.println("除數為零");
+        } else {
+            int ans = num/den;
+            System.out.println("end of main()!!");
+        }
+    }
+}
